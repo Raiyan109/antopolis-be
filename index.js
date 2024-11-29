b2350db09a9e496a48d803d923ed633ec0482c95
Raiyan109 <kraiyan109@gmail.com>
2024-11-29 11:23:43 +0600

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config()
+const Animal = require('./models/animal/animalModel.js')
 
 const app = express()
 
@@ -9,13 +10,46 @@ app.use(express.json())
 app.use(cors())
 
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`Server listening on ${process.env.PORT}`)
+app.get('/', async (req, res) => {
+    const animals = await Animal.find()
+
+    if (!animals) {
+        return res.status(400).json({ msg: 'No Animal found' })
+    }
+
+    res.status(200).json({
+        success: true,
+        data: animals
+    })
 })
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+
+app.post('/create', async (req, res) => {
+    const { name, status, desc, dueTask } = req.body
+
+    try {
+        const task = await Task.create({ name, status, desc, dueTask })
+
+        if (!task) {
+            return res.status(400).json({ msg: 'No Task can be created' })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: task
+        })
+    } catch (error) {
+
+        return res.status(400).json({ msg: error.message })
+    }
+})
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => {
+        app.listen(process.env.PORT, (req, res) => {
+            console.log(`Server listening on ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
